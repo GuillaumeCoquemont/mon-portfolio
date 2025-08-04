cf75f6b8abfa737d8c2dd36d0f6bd5c65bb3e204
Guillaume Coquemont <gui.coquemont@gmail.com>
2025-08-04 11:07:09 +0200

    Création de la page projet avec les différents projet, suite à faire : gérer le projet Cyna qui reste a héberger avant de le terminer sur cette page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,7 +1,15 @@
-    export default function Projects() {
+import ProjectsIntro from "../components/Project/ProjectsIntro";
+import ProjectsGrid from "../components/Project/ProjectsGrid";
+
+export default function Projects() {
   return (
-    <div>
-      <h1>Page Projects</h1>
+    <div className="scroll-smooth md:snap-y md:snap-mandatory overflow-y-scroll h-screen">
+      <section className="md:snap-start sm:min-h-screen">
+        <ProjectsIntro />
+      </section>
+      <section className="md:snap-start sm:min-h-screen">
+        <ProjectsGrid />
+      </section>
     </div>
-  )
-}
+  );
+} 
